37cb185afdb46e2e407a88da2f0e0a6c42f72065
angus2025 <angus.workspace@gmail.com>
2025-12-05 14:48:11 +0800

    Remove unused import of os module

--- app.py
+++ app.py
@@ -99,6 +99,5 @@ def speak():
 #    app.run(port=5000, threaded=True)
     
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
